f4d119e752be6a41dfeba30bc9436cc57b703a0d
Michael <michael.ajanaku@xprizo.com>
2026-04-21 09:53:32 +0100

    add speed insights

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AuthStore } from './core/store/auth.store';
 import { ToastComponent } from './shared/components/toast/toast';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastComponent],
+  imports: [RouterOutlet, ToastComponent,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  ngOnInit(): void {
+    injectSpeedInsights();
+  }
+
 
   protected title = 'house-padi';
-
 
 
 }
